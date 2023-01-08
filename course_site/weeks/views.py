d801853808ec231845d043403563aacbb5b48439
Carlos Cardona <cargicar@gmail.com>
2023-01-08 10:20:16 -0500

    Original Commit
    Original commit from local

---
+++ course_site/weeks/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
+
+# Create your views here.
+def home(request):
+    return TemplateResponse(request,"weeks/home.html",)
+    #return (request, 'homeApp/home.html')
+
+def syllabus(request):
+    return TemplateResponse(request,"weeks/syllabus.html",)
+
+
+def class01(request):
+     return TemplateResponse( request,"weeks/class01.html",)
+
+
+def class00(request):
+     return TemplateResponse( request,"weeks/class00.html",)
+
